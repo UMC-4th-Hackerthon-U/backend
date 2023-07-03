ac033f92ee8048662bfc6129f2ee7ad444d3451e
Sewoong <lympsw12@naver.com>
2023-07-03 18:54:24 +0900

    BaseTimeEntity update

--- src/main/java/com/example/kicking/utils/BaseTimeEntity.java
+++ src/main/java/com/example/kicking/utils/BaseTimeEntity.java
@@ -16,8 +16,8 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
     @CreatedDate
     @Column(name = "created_at")
-    private LocalDateTime localDateTime;
+    private LocalDateTime createdDate;
     @LastModifiedDate
     @Column(name = "updated_at")
-    private LocalDateTime modifiedDate;
+    private LocalDateTime updatedDate;
 }
