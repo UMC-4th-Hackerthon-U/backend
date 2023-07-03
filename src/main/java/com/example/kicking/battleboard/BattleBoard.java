cef2deeae21e3d68d6e60fb448450daa12701f6b
Sewoong <lympsw12@naver.com>
2023-07-03 17:16:06 +0900

    feature/ BaseTimeEntity 설정

--- src/main/java/com/example/kicking/battleboard/BattleBoard.java
+++ src/main/java/com/example/kicking/battleboard/BattleBoard.java
@@ -1,10 +1,11 @@
 package com.example.kicking.battleboard;
 
 import com.example.kicking.board.Board;
+import com.example.kicking.utils.BaseTimeEntity;
 import jakarta.persistence.*;
 
 @Entity
-public class BattleBoard {
+public class BattleBoard extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
