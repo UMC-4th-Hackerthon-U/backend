70388415c3fd20a83a7cb3f4af25cd13ee90f24b
SewoongPark <lympsw12@naver.com>
2023-07-04 01:21:59 +0900

    Dev heron (#5)
    * feature/ story paging 처리
    
    * feature/ Board API
    
    * feature/ Board API

--- src/main/java/com/example/kicking/board/BoardController.java
+++ src/main/java/com/example/kicking/board/BoardController.java
@@ -1,10 +1,12 @@
 package com.example.kicking.board;
 
+import com.example.kicking.board.dto.BoardGetDto;
 import com.example.kicking.board.dto.BoardGetReqDto;
 import com.example.kicking.board.dto.BoardGetResDto;
 import com.example.kicking.story.dto.StoryGetResDto;
 import com.example.kicking.utils.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +20,35 @@ import java.util.List;
 public class BoardController {
     private final BoardService boardService;
 
+    /**
+     * 흑역사 추가
+     * @param multipartFiles
+     * @param boardGetReqDto
+     * @return
+     */
     @PostMapping("/board")
     public BaseResponse<String> insertBoard(@RequestPart(value = "image") List<MultipartFile> multipartFiles,
                                             @RequestPart(value = "boardGetReqDto") BoardGetReqDto boardGetReqDto){
         return new BaseResponse<>(boardService.insertBoard(multipartFiles,boardGetReqDto));
     }
 
+    /**
+     * 모든 흑역사 조회(Paging)
+     * @param page
+     * @return
+     */
     @GetMapping("/board")
     public BaseResponse<Page<BoardGetResDto>> selectAllBoard(@Param("page") int page){
-        return new BaseResponse<>(boardService.selectBoard(page));
+        return new BaseResponse<>(boardService.selectAllBoard(page));
     }
 
+    /**
+     * 흑역사 상세 조회
+     * @param id
+     * @return
+     */
+    @GetMapping("/board/{id}")
+    public BaseResponse<BoardGetDto> selectBoard(@PathVariable Long id){
+        return new BaseResponse<>(boardService.selectBoard(id));
+    }
 }
--- src/main/java/com/example/kicking/board/BoardService.java
+++ src/main/java/com/example/kicking/board/BoardService.java
@@ -1,6 +1,7 @@
 package com.example.kicking.board;
 
 
+import com.example.kicking.board.dto.BoardGetDto;
 import com.example.kicking.board.dto.BoardGetReqDto;
 import com.example.kicking.board.dto.BoardGetResDto;
 import com.example.kicking.boardImage.BoardImage;
@@ -39,10 +40,15 @@ public class BoardService {
         return "흑역사 추가 완료";
     }
 
-    public Page<BoardGetResDto> selectBoard(int page){
+    public Page<BoardGetResDto> selectAllBoard(int page){
         PageRequest pageRequest = PageRequest.of(page, 3, Sort.by(Sort.Direction.DESC, "createdDate"));
         Page<Board> results = boardRepository.findAll(pageRequest);
         return results.map(BoardGetResDto::new);
+    }
+
+    public BoardGetDto selectBoard(Long boardId){
+        Board board = boardRepository.findById(boardId).orElseThrow();
+        return new BoardGetDto(board);
     }
 
 }
